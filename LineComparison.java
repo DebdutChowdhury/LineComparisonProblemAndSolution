a0b4ba4d0cd72d23d56e33d43be9eaecedc537f4
DebdutChowdhury <debdut.chowdhury2018@gmail.com>
2021-02-05 20:31:24 +0530

    Added: calcuale line of length here

--- LineComparison.java
+++ LineComparison.java
@@ -1,5 +1,16 @@
-public class LineComparison{
-	public static void main(String[] args){
-		System.out.println("Welcome to my Line Comparison problem and solution");
+public class LineComparison {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x1 = 2;
+		int x2 = 4;
+		int y1 = 5;
+		int y2 = 6;
+		
+		double lineLength = 0;
+		lineLength = Math.sqrt( (x2 - x1) ^ 2 + (y2- y1) ^ 2);
+		System.out.println("Length of line: "+lineLength);
 	}
+
 }
+
