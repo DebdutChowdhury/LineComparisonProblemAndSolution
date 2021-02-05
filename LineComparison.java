babd8c6e14581bd187a5a474069b46574a5a3950
DebdutChowdhury <debdut.chowdhury2018@gmail.com>
2021-02-05 20:51:58 +0530

    Added: here use equal method to check lengths of two line

--- LineComparison.java
+++ LineComparison.java
@@ -6,10 +6,27 @@ public class LineComparison {
 		int x2 = 4;
 		int y1 = 5;
 		int y2 = 6;
+		int x3 = 3;
+		int x4 = 7;
+		int y3 = 8;
+		int y4 = 9;
 		
-		double lineLength = 0;
-		lineLength = Math.sqrt( (x2 - x1) ^ 2 + (y2- y1) ^ 2);
-		System.out.println("Length of line: "+lineLength);
+		double lineLength1 = 0;
+		double lineLength2 = 0;
+		lineLength1 = Math.sqrt( (x2 - x1) ^ 2 + (y2- y1) ^ 2);
+		lineLength2 = Math.sqrt( (x4 - x3) ^ 2 + (y4- y3) ^ 2);
+
+		System.out.println("1st Length of line: "+lineLength1);
+		System.out.println("1st Length of line: "+lineLength2);
+		
+		String l1 = String.valueOf(lineLength1);
+		String l2 = String.valueOf(lineLength2);
+		
+		boolean result = l1.equals(l2);
+		if (result == true)
+			System.out.println("Both lengths are equal that means the lines are same length.");
+		else
+			System.out.println("It's lengths are not equal.");
 	}
 
 }
